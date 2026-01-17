8148d07729a21d71d80d1a9729526247cb4829c0
screadapp <scread.app@gmail.com>
2026-01-18 05:17:09 +0530

    Add /health endpoint for robust healthchecks

--- gutendex/urls.py
+++ gutendex/urls.py
@@ -9,7 +9,10 @@ from books import views
 router = routers.DefaultRouter()
 router.register(r'books', views.BookViewSet)
 
+from django.http import HttpResponse
+
 urlpatterns = [
+    re_path(r'^health/?$', lambda request: HttpResponse("OK")),
     re_path(r'^$', TemplateView.as_view(template_name='home.html')),
     re_path(r'^', include(router.urls)),
 ]
